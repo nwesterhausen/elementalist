b0a225c4cab671a2ef2844351eb2dae54186e09a
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2024-01-10 14:47:01 -0500

    wip: reference code for how to zoom camera

--- game/src/camera/creation.rs
+++ game/src/camera/creation.rs
@@ -4,3 +4,12 @@ use bevy::prelude::*;
 pub fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
+
+// /// Zooms in on the camera (how to do this for future reference)
+// pub fn zoom_in(mut query: Query<&mut OrthographicProjection, With<Camera>>, time: Res<Time>) {
+//     for mut projection in query.iter_mut() {
+//         projection.scale -= 0.1 * time.delta_seconds();
+
+//         println!("Current zoom scale: {}", projection.scale);
+//     }
+// }

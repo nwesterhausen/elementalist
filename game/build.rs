dc77e171636d0a856e0d27a0f7f682be257ef297
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2024-01-11 13:16:31 -0500

    build: update app icon in windows

--- game/build.rs
+++ game/build.rs
@@ -1,3 +1,11 @@
+extern crate embed_resource;
+
 fn main() {
+    // Add built.rs build-time information to the binary
     built::write_built_file().expect("Failed to acquire build-time information");
+    // Set the program icon
+    let target = std::env::var("TARGET").unwrap();
+    if target.contains("windows") {
+        embed_resource::compile("./icon.rc", embed_resource::NONE);
+    }
 }

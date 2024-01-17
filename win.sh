78cd82c17fac0367f8f5afb392a94d3a63239221
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2024-01-17 09:44:14 -0500

    chore: delete win.sh
    Never worked right, so removing it from the repo

--- win.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-## The recommended way to run your Bevy app from WSL is to cross-compile for Windows.
-
-cargo build --target x86_64-pc-windows-msvc &&
-cp target/x86_64-pc-windows-msvc/debug/elementalist.exe . &&
-exec ./elementalist.exe "$@"

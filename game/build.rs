a8fad618df7f72e0f9428e77390b6c08dd2924a2
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2024-01-23 08:53:51 -0500

    build: modify build script to more explicitly affect windows

--- game/build.rs
+++ game/build.rs
@@ -3,17 +3,15 @@
 //! This script is run before the game binary is compiled. It is used to set the program icon and
 //! to add build-time information to the binary.
 
+#[cfg(target_os = "windows")]
 extern crate embed_resource;
 
 fn main() {
     // Add built.rs build-time information to the binary
     built::write_built_file().expect("Failed to acquire build-time information");
-    // Set the program icon
-    let Ok(target) = std::env::var("TARGET") else {
-        println!("Failed to get target triple, cannot set program icon");
-        return;
-    };
-    if target.contains("windows") {
+    // Set the program icon (Windows only)
+    #[cfg(target_os = "windows")]
+    {
         embed_resource::compile("./assets/icon.rc", embed_resource::NONE);
     }
 }

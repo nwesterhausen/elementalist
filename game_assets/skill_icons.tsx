c4764c6eef75a8125f4d68fd036e3071b4f499ea
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2024-01-28 11:17:52 -0500

    feat: add skill icon tileset

---
+++ game_assets/skill_icons.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="skill_icons" tilewidth="32" tileheight="32" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="Skills/Aeromancy.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="Skills/Chronomancy.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="Skills/Cryomancy.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="Skills/Fulgomancy.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="Skills/Geomancy.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="Skills/Hydromancy.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="Skills/Necromancy.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="Skills/Pyromancy.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="Skills/Trudomancy.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="Placeholder.png"/>
+ </tile>
+</tileset>

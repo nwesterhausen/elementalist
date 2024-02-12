a8e6c2da9ecb163854aad868cc41e6f958360f2f
Nick Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2024-02-12 14:37:00 -0500

    wip: skill book ui

--- game/src/game_overlays/plugin.rs
+++ game/src/game_overlays/plugin.rs
@@ -2,13 +2,14 @@ use bevy::prelude::*;
 
 use game_library::state::Overlay;
 
-use super::status_screen::StatusScreenPlugin;
+use super::{skill_book::SkillBookUiPlugin, status_screen::StatusScreenPlugin};
 
 /// Plugin for the status screen
 pub struct GameOverlaysPlugin;
 
 impl Plugin for GameOverlaysPlugin {
     fn build(&self, app: &mut App) {
-        app.add_state::<Overlay>().add_plugins(StatusScreenPlugin);
+        app.add_state::<Overlay>()
+            .add_plugins((StatusScreenPlugin, SkillBookUiPlugin));
     }
 }

922380c7792021f1a52b0a3dfe0b1fe3fa92ffc3
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2026-01-09 10:34:18 -0500

    checkingin

--- game_library/src/noise/generator.rs
+++ game_library/src/noise/generator.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use noise::{NoiseFn, Perlin, Seedable};
 use rand::Rng;
 
 use crate::state::Game;
@@ -24,4 +25,18 @@ pub(super) fn generate_new_seed(mut seed: ResMut<GenerationSeed>) {
 pub(super) fn progress_to_playing(mut state: ResMut<NextState<Game>>) {
     state.set(Game::Playing);
     tracing::info!("Progressing to playing state");
+}
+
+/// Using the seed and noise, generate the world.
+///
+/// This system should be run when the game state is `Game::Generating`.
+#[allow(clippy::needless_pass_by_value)]
+pub(super) fn generate_world(seed: Res<GenerationSeed>, mut commands: Commands) {
+    tracing::info!("Generating world with seed: {}", seed.0);
+
+    let perlin = Perlin::new(seed.0);
+
+    let origin = perlin.get([0.0, 0.0]);
+
+    tracing::info!("Origin: {}", origin);
 }
--- game_library/src/noise/resources.rs
+++ game_library/src/noise/resources.rs
@@ -2,4 +2,4 @@ use bevy::prelude::*;
 
 /// A resource that stores the seed for the generation of the primal realm.
 #[derive(Debug, Default, Clone, Eq, PartialEq, Hash, Resource)]
-pub struct GenerationSeed(pub u64);
+pub struct GenerationSeed(pub u32);
